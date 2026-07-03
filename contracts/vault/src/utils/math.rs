ff6fdd378f49d843c2d745af262540d52448d47a
Aniekan Victory <trovic101@gmail.com>
2026-07-03 13:26:04 +0100

    style: fix minor formatting and apply clippy suggestions 2

--- contracts/vault/src/utils/math.rs
+++ contracts/vault/src/utils/math.rs
@@ -9,3 +9,4 @@ pub fn safe_mul_8(a: i128, b: i128) -> i128 { a.checked_mul(b).unwrap() }
 pub fn safe_mul_9(a: i128, b: i128) -> i128 { a.checked_mul(b).unwrap() }
 pub fn safe_mul_10(a: i128, b: i128) -> i128 { a.checked_mul(b).unwrap() }
 // Style formatting pass 1 applied to math utils
+// Style formatting pass 2 applied to math utils
