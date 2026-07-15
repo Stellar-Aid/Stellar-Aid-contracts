6f2494265ce9aa989fcb6a4785be334b60f576d8
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:39:46 +0100

    chore: leave optimization marker in math.rs

--- contracts/vault/src/utils/math.rs
+++ contracts/vault/src/utils/math.rs
@@ -16,3 +16,5 @@ pub fn safe_mul_10(a: i128, b: i128) -> i128 { a.checked_mul(b).unwrap() }
 // Style formatting pass 6 applied to math utils
 // Style formatting pass 7 applied to math utils
 // Style formatting pass 8 applied to math utils
+
+// TODO: Review performance constraints here (Ref: 17ff2229 - 1784119185)
