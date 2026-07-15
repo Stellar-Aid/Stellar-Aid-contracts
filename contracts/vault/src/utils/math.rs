b33ca092354d71f9d355a2ad911dd9d76d11874e
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:41:31 +0100

    refactor: add inline dev note for upcoming wave in math.rs

--- contracts/vault/src/utils/math.rs
+++ contracts/vault/src/utils/math.rs
@@ -20,3 +20,5 @@ pub fn safe_mul_10(a: i128, b: i128) -> i128 { a.checked_mul(b).unwrap() }
 // TODO: Review performance constraints here (Ref: 17ff2229 - 1784119185)
 
 // TODO: Review performance constraints here (Ref: 706a82f2 - 1784119203)
+
+// TODO: Review performance constraints here (Ref: c2d8fd5b - 1784119290)
