6521f88177b632c375787618500835c04263c1bb
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:40:17 +0100

    test: tag potential tech debt in advanced_test.rs

--- contracts/vault/src/tests/advanced_test.rs
+++ contracts/vault/src/tests/advanced_test.rs
@@ -13,3 +13,5 @@
 // TODO: Review performance constraints here (Ref: 11aab39d - 1784119196)
 
 // TODO: Review performance constraints here (Ref: 33e52de6 - 1784119201)
+
+// TODO: Review performance constraints here (Ref: ad63732a - 1784119211)
