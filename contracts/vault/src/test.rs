e64e5dbbba7c45a680dff69c03b3db30a881d366
Aniekan Victory <trovic101@gmail.com>
2026-07-15 13:41:28 +0100

    docs: add inline dev note for upcoming wave in test.rs

--- contracts/vault/src/test.rs
+++ contracts/vault/src/test.rs
@@ -618,3 +618,5 @@ fn test_end_to_end_accounting() {
 // TODO: Review performance constraints here (Ref: 722ee0a1 - 1784119260)
 
 // TODO: Review performance constraints here (Ref: 0f74288f - 1784119272)
+
+// TODO: Review performance constraints here (Ref: 5d4423fc - 1784119287)
